b317076f8ba964563410804eac3ac9ea1e5ef050
Artem Blodov <mr.samy@rambler.ru>
2024-02-17 12:03:08 +0200

    Initial commit

---
+++ newsletter/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+
+from newsletter.apps import NewsletterConfig
+
+
+app_name = NewsletterConfig.name
+
+urlpatterns = [
+    # path('', ..., name='newsletter'),
+
+]
