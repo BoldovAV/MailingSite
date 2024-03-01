18d2df433324fcb9fca024821d327567f4136add
Artem Blodov <mr.samy@rambler.ru>
2024-03-01 12:49:39 +0200

    home

--- home/views.py
+++ home/views.py
@@ -1,5 +1,22 @@
+import random
+
 from django.shortcuts import render
+
+from blog.models import Blog
+from newsletter.models import Newsletter, Client
 
 
 def home(request):
-    return render(request, 'home/home.html', {'title': 'home'})
+    blog_all = list(Blog.objects.all())
+    print(blog_all)
+    blog_list_home = random.sample(blog_all, k=3)
+
+    context = {
+        'title': 'Домашняя страница',
+        'object_all': len(Newsletter.objects.all()),
+        'object_is_active': len(Newsletter.objects.filter(is_active=True)),
+        'client_all': len(Client.objects.all()),
+        'blog_list': blog_list_home
+
+    }
+    return render(request, 'home/home.html', context)
