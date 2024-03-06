4f6170387f496e76a254d7c1f5a2f4a84342c9a2
Artem Blodov <mr.samy@rambler.ru>
2024-03-06 13:02:58 +0200

    task rework

--- newsletter/tasks.py
+++ newsletter/tasks.py
@@ -33,34 +33,66 @@ def mail_process(*args, **kwargs):
 
 
 def check_newsletter_status():
-    current_date = datetime.now().date()
-    current_time = datetime.now().time()
+    current_datetime = datetime.now()
+    current_day_delta = timedelta(days=current_datetime.day)
+    current_time_delta = timedelta(hours=current_datetime.hour, minutes=current_datetime.minute)
     print('мы тут')
-    print(type(Newsletter))
     newsletter_all = Newsletter.objects.filter(
         Q(status__in=(0, 1)),
         Q(is_active=True)
     )
+    print(newsletter_all.objects.all())
+    print('и мы тут')
     need_send = []
     for news in newsletter_all:
-        if (0 < (news.time_to_send - current_time).total_seconds() < 60 and
-                news.period_start <= current_date <= news.period_fin):
+        send_time = timedelta(hours=news.time_to_send.hour, minutes=news.time_to_send.minute)
+        if (0 < (send_time - current_time_delta).total_seconds() < 60 and
+                news.period_start <= current_datetime.date() <= news.period_fin):
             news.status = 1
             news.save()
-            if news.period in ['MINU', 'DAYS']:
+            print(news.period)
+            if news.period == 'раз в минуту (на тесты)':
+                print('Hiiii')
                 need_send.append(news)
-            elif news.period == 'WEEK' and (current_date - news.last_try.total_seconds()/86400) == 7:
+            elif news.period == 'раз в день' and (current_datetime - timedelta(days=news.last_try.day)/86400) == 1:
                 need_send.append(news)
-            elif news.period == 'MONT' and (current_date - news.last_try.total_seconds()/86400) == 30:
+            elif news.period == 'раз в неделю' and (current_datetime - timedelta(days=news.last_try.day)/86400) == 7:
                 need_send.append(news)
-        elif (news.time_to_send < current_time and
-              news.period_fin <= current_date):
-            news.status = 2
-            news.save()
+            elif news.period == 'раз в месяц' and (current_day_delta - timedelta(days=news.last_try.day)/86400) == 30:
+                need_send.append(news)
+            elif (news.time_to_send < current_datetime.time() and
+                  news.period_fin <= current_datetime.date()):
+                news.status = 2
+                news.save()
     return need_send
 
-
 # Старая логика:
+
+# current_day_delta = timedelta(days=current_datetime.day)
+# current_time_delta = timedelta(hours=current_datetime.hour, minutes=current_datetime.minute)
+
+
+#     for news in newsletter_all:
+#         send_time = timedelta(hours=news.time_to_send.hour, minutes=news.time_to_send.minute)
+#         if (0 < (send_time - current_time_delta).total_seconds() < 60 and
+#                 news.period_start <= current_datetime.date() <= news.period_fin):
+#             news.status = 1
+#             news.save()
+#             if news.period in ['MINU', 'DAYS']:
+#                 need_send.append(news)
+#             elif news.period == 'WEEK' and (current_datetime - timedelta(news.last_try) == 7:
+#                 need_send.append(news)
+#             elif news.period == 'MONT' and (current_day_delta - timedelta(days=news.last_try.day)) == 0:
+#                 need_send.append(news)
+#             elif (news.time_to_send < current_datetime.time()) and
+#                     news.period_fin <= current_datetime.date()):
+#                 news.status = 2
+#             news.save()
+
+
+# current_date = datetime.now().date()
+# current_time = datetime.now().time()
+
 # for news in newsletter_all:
 #     if (news.time_to_send <= current_time <= (news.time_to_send + timedelta(minutes=1)).time() and
 #             news.period_start <= current_date <= news.period_fin):
@@ -80,12 +112,17 @@ def check_newsletter_status():
 #         news.save()
 
 
-# start = datetime(2024, 2, 16).date()
-# seicas = datetime.now().date()
+# start = datetime(2024, 2, 16, 16, 15, 00)
+# start2 = datetime(2024, 2, 16, 16, 15, 00).month
+# seicas = datetime.now().time()
+# tm1 = timedelta(hours=start.hour, minutes=start.minute)
+# tm2 = timedelta(hours=seicas.hour, minutes=seicas.minute)
 # print(start)
+# print(start2)
+#
 # print(seicas)
-# raznica = (seicas - start).days % 7
-# dilim = raznica % 7
+# raznica = (tm1 - tm2).total_seconds()/60
+# # dilim = raznica % 7
 #
 # print(raznica)
 # print(dilim)
@@ -95,3 +132,12 @@ def check_newsletter_status():
 # print(type(news))
 
 # (news.time_to_send + timedelta(minutes=1)).time()
+
+# seicas = datetime.now()
+# datetime_some = datetime(year=2024, month=1, day=7, hour=12, minute=10, second=00)
+# day_some_date = timedelta(days=datetime_some.day)
+# current_day_delta = timedelta(days=seicas.day)
+# current_time_delta = timedelta(hours=seicas.hour, minutes=seicas.minute)
+# print(seicas)
+# print((seicas - datetime_some))
+# print((seicas + relativedelta(days=7)).date())
